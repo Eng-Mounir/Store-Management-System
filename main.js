72d35875706e3c8d524bb0fa2d6576e43b0acba4
Eng-Mounir <mouniramir39@gmail.com>
2025-10-15 17:46:06 +0300

    add addProduct part1

---
+++ main.js
@@ -0,0 +1,14 @@
+var productName = document.getElementById("productName");
+var productPrice = document.getElementById("productPrice");
+var productCategory = document.getElementById("productCategory");
+var productDescription = document.getElementById("productDescription");
+
+function addProduct() {
+    console.log(productName.value);
+    console.log(productPrice.value);
+    console.log(productCategory.value);
+    console.log(productDescription.value);
+    
+}
+
+
