09368e56e225d91a1e07f2075a59e620797d7d7a
Eng-Mounir <mouniramir39@gmail.com>
2025-10-15 18:23:25 +0300

    add addProduct part2

--- main.js
+++ main.js
@@ -3,11 +3,22 @@ var productPrice = document.getElementById("productPrice");
 var productCategory = document.getElementById("productCategory");
 var productDescription = document.getElementById("productDescription");
 
+//ha3ml productList 3ashan lw 3amlt save aktr mn product may3mloosh override 3ala b3d f asagylo f array ykoon 3andy 
+var productList = []; //dh nafs ely bygyly mn backend array of objectsss
+
+//3amlt function dy 3ashan a cal it or fire it lama (onclick)byta3 button el html
 function addProduct() {
-    console.log(productName.value);
-    console.log(productPrice.value);
-    console.log(productCategory.value);
-    console.log(productDescription.value);
+    //3amlt object 3ashan a group related varaibles dool gowah fy memory w a3raf a call them aw acess on them anytime
+    var product = {
+        name:productName.value,
+        price:productPrice.value,
+        category:productCategory.value,
+        description:productDescription.value
+    }
+
+    //ha3ml push fy array byta3y 3asahn y save products fyh
+    productList.push(product);
+    console.log(productList);
     
 }
 
